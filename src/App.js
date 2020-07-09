de696374846ce462c37fb4d2be50fb3f8fd630d1
Ravalika <25775803+ravalikab@users.noreply.github.com>
2020-07-10 00:06:42 +0530

    Basic counter app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,39 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import Button from "@material-ui/core/Button";
+import Container from "@material-ui/core/Container";
+import Box from "@material-ui/core/Box";
+
+import Typography from "@material-ui/core/Typography";
 
 function App() {
+  const [value, setValue] = useState(1);
+
+  const handleAddClick = () => {
+    setValue(value + 1);
+  };
+  const handleSubtractClick = () => {
+    setValue(value - 1);
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+    <div>
+      <Container maxWidth="sm">
+        <Typography variant="h1" gutterBottom>
+          {value}
+        </Typography>
+        <Button variant="outlined" color="primary" onClick={handleAddClick}>
+          Add
+        </Button>
+        <Box m={1} />
+        <Button
+          variant="outlined"
+          color="primary"
+          onClick={handleSubtractClick}
         >
-          Learn React
-        </a>
-      </header>
+          Subtract
+        </Button>
+      </Container>
     </div>
   );
 }
