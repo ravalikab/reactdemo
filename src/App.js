267892b22b399d2e6dcc7fe5640f52bcb791d425
Ravalika <25775803+ravalikab@users.noreply.github.com>
2020-07-15 22:07:25 +0530

    to do app

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React, { useState } from "react";
-import logo from "./logo.svg";
 import "./App.css";
 import TextField from "@material-ui/core/TextField";
 import Button from "@material-ui/core/Button";
-const list = [];
+import List from "@material-ui/core/List";
+import ListItem from "@material-ui/core/ListItem";
+import ListItemText from "@material-ui/core/ListItemText";
+import Checkbox from "@material-ui/core/Checkbox";
+import ListItemIcon from "@material-ui/core/ListItemIcon";
+import ClearIcon from "@material-ui/icons/Clear";
+import { Typography } from "@material-ui/core";
 
 function App() {
   const [text, setText] = useState("");
@@ -11,28 +16,85 @@ function App() {
   const handleChange = (e) => {
     setText(e.target.value);
   };
-  const handleClick = (e) => {
-    setListValue(listValue.concat(text));
+  const handleClick = () => {
+    setListValue(listValue.concat({ text: text, isCompleted: false }));
     setText("");
+  };
+  const handleClose = (index) => {
+    listValue.splice(index, 1);
+    setListValue([...listValue]);
+  };
+  const keyPress = (e) => {
+    if (e.keyCode === 13) {
+      handleClick();
+    }
+  };
+  const checkboxValue = (e, index) => {
+    listValue[index].isCompleted = e.target.checked;
+    setListValue([...listValue]);
   };
 
   return (
-    <div>
+    <div className="container">
       <TextField
+        className="textfield"
         onChange={handleChange}
+        onKeyDown={keyPress}
         id="outlined-basic"
-        label="Outlined"
+        label="to-do"
         variant="outlined"
         value={text}
+        size="small"
       />
       <Button onClick={handleClick} variant="contained" color="secondary">
         Submit
       </Button>
-      <ul>
-        {listValue.map((item) => (
-          <li key={item}>{item}</li>
-        ))}
-      </ul>
+      <List>
+        {listValue.map((item, index) => {
+          if (!item.isCompleted)
+            return (
+              <ListItem className="listitem" key={index}>
+                <Checkbox
+                  checked={item.isCompleted}
+                  onChange={(e) => checkboxValue(e, index)}
+                  inputProps={{ "aria-label": "uncontrolled-checkbox" }}
+                />
+                <ListItemText primary={item.text} />
+                <ListItemIcon>
+                  <ClearIcon
+                    onClick={() => {
+                      handleClose(index);
+                    }}
+                  />
+                </ListItemIcon>
+              </ListItem>
+            );
+        })}
+      </List>
+      <Typography variant="h4">Completed</Typography>
+
+      <List className="strikethrough">
+        {listValue.map((item, index) => {
+          if (item.isCompleted)
+            return (
+              <ListItem className="listitem" key={index}>
+                <Checkbox
+                  checked={item.isCompleted}
+                  onChange={(e) => checkboxValue(e, index)}
+                  inputProps={{ "aria-label": "uncontrolled-checkbox" }}
+                />
+                <ListItemText primary={item.text} />
+                <ListItemIcon>
+                  <ClearIcon
+                    onClick={() => {
+                      handleClose(index);
+                    }}
+                  />
+                </ListItemIcon>
+              </ListItem>
+            );
+        })}
+      </List>
     </div>
   );
 }
